efbf383eefb92e58987b01d3f182b5086d903f7c
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2025-11-19 10:12:04 +0100

    added logout

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,13 @@
 import Button from "../components/button";
 import CounterBtn from "../components/CounterBtn";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function Home() {
     const [items, setItems] = useState([])
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
 
     useEffect(() => {
         const fetchData = async () => {
@@ -18,8 +21,42 @@ export default function Home() {
 
         fetchData()
     }, [])
+
+    const handleLogout = async() => {
+        setLoading(true)
+        try{
+            const res = await fetch('https://nodeclass-batch3.vercel.app/logout', {
+                method: "GET",
+                credentials: "include"
+            })
+
+            if(!res.ok){
+                toast.error("Logout failed", {
+                    position: "top-right",
+                    autoClose: 5000,
+                    theme: "dark"
+                })
+            }
+
+            const data = await res.json()
+            console.log(data)
+            toast.success("Logout successful", {
+                position: "top-right",
+                autoClose: 5000,
+                theme: "dark"
+            })
+
+            // navigate('/login')
+            
+        }catch(err){
+            console.error(err)
+        }finally{
+            setLoading(false)
+        }
+    }
     return(
         <div>
+        <ToastContainer/>
     <div>
     <h1>Hello Vite + React!</h1>
     <ul>
@@ -34,6 +71,12 @@ export default function Home() {
     <Link to={'/about'}>
         Go to About Page
     </Link>
+
+    <button className="text-white bg-red-600 p-4 cursor-pointer rounded-3xl w-[10rem]"
+    onClick={handleLogout}
+    >
+        {loading ? 'Logging out.... please wait' : 'Logout'}
+    </button>
 
     <section className="w-full mt-20 flex flex-col items-center justify-center">
             {items.map((item, index) => (
